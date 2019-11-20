0e9c33ec21380a817db198fe3af79c98724bd303
s-fletcher <sfletchertx@gmail.com>
2019-11-19 21:03:31 -0600

    Organizing environment

---
+++ src/components/Title/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+class Title extends React.Component {
+    render () {
+        return (
+            <div className="Title">
+                <p>Sam Fletcher</p>
+            </div>
+        );
+    }
+}
+
+export default Title;
