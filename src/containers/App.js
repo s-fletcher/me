4016ee55ec9e092cc9b50ed9c899dd3d627bb24f
s-fletcher <sfletchertx@gmail.com>
2019-12-02 13:57:06 -0600

    implemented console

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.scss';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
-// import Console from '../components/Console';
+import Console from '../components/Console';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
-      {/* <Console /> */}
+      <Console />
     </div>
   );
 }
