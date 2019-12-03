d0001e617d61cc16235fe2458b6e2d1d217262bc
s-fletcher <sfletchertx@gmail.com>
2019-12-03 14:31:47 -0600

    added projects without json

---
+++ src/components/Projects/Featured/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './index.scss';
+import { FaGithub } from 'react-icons/fa'
+import { FaExternalLinkAlt } from 'react-icons/fa'
+
+class Featured extends React.Component {
+
+    github() {
+        if (this.props.github !== "")
+            return(
+                <FaGithub className="item" />
+            );
+    }
+
+    external() {
+        if (this.props.external !== "")
+            return (
+                <FaExternalLinkAlt className="item" />
+            );
+    }
+
+    render () {
+        return (
+            <div className={"Featured " + this.props.align}>
+                <div className="thumbnail">
+                    <img src={this.props.thumbnail} alt="thumbnail" />
+                </div>
+                <div className="content">
+                    <p className="featured">Featured Project</p>
+                    <p className="title">{this.props.title}</p>
+                    <div className="description">
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className="tech-stack">
+                        {this.props.techStack.map((item, key) =>
+                            <p className="item" key={key}>{item}</p>
+                        )}
+                    </div>
+                    <div className="links">
+                        <a href={this.props.external}>{this.external()}</a>
+                        <a href={this.props.github}>{this.github()}</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Featured;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import Console from '../components/Console';
+import Featured from '../components/Projects/Featured';
 
 function App() {
   return (
@@ -10,6 +11,22 @@ function App() {
       <Navbar />
       <Header />
       <Console />
+      <Featured 
+        align="left" 
+        thumbnail={require("../images/hacklahoma-registration-thumbnail.png")}
+        title="Hacklahoma Registration" 
+        description="A heavily edited version of Quill, a registration system for attendees to register for a hackathon. This system was built for Hacklahoma and could be called Quill 2.0"
+        techStack={["Node.js", "Heroku", "Mongo"]}
+        github="https://www.google.com"
+        external="https://www.google.com"  />
+      <Featured
+        align="right"
+        thumbnail={require("../images/hacklahoma-registration-thumbnail.png")}
+        title="Hacklahoma Registration"
+        description="A heavily edited version of Quill, a registration system for attendees to register for a hackathon. This system was built for Hacklahoma and could be called Quill 2.0"
+        techStack={["Node.js", "Heroku", "Mongo"]}
+        github="https://www.google.com"
+        external="https://www.google.com" />
     </div>
   );
 }
