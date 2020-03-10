e934cd86ec22e8023194aa2358ccffd083a7d322
s-fletcher <sfletchertx@gmail.com>
2020-03-10 17:17:29 -0500

    Updated projects, and open to new tab

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -60,13 +60,19 @@ class Featured extends React.Component {
                         <p>{this.props.description}</p>
                     </div>
                     <div className="techStack">
-                        {this.props.techStack.map((item, key) =>
-                            <p className="item" key={key}>{item}</p>
-                        )}
+                        {this.props.techStack.map((item, key) => (
+                            <p className="item" key={key}>
+                                {item}
+                            </p>
+                        ))}
                     </div>
                     <div className="links">
-                        <a href={this.props.github}>{this.github()}</a>
-                        <a href={this.props.external}>{this.external()}</a>
+                        <a href={this.props.github} target="_blank" rel="noopener noreferrer">
+                            {this.github()}
+                        </a>
+                        <a href={this.props.external} target="_blank" rel="noopener noreferrer">
+                            {this.external()}
+                        </a>
                     </div>
                 </div>
             </div>
--- src/components/Projects/Project/index.js
+++ src/components/Projects/Project/index.js
@@ -1,42 +1,36 @@
-import React from 'react';
-import './index.scss';
-import { FaGithub } from 'react-icons/fa'
-import { IoMdOpen } from 'react-icons/io'
-import { IoIosGlobe } from 'react-icons/io'
-import { IoIosVideocam } from 'react-icons/io'
-import { FaCogs } from 'react-icons/fa'
-import { FaTrophy } from 'react-icons/fa'
-import ReactTooltip from 'react-tooltip'
+import React from "react";
+import "./index.scss";
+import { FaGithub } from "react-icons/fa";
+import { IoMdOpen } from "react-icons/io";
+import { IoIosGlobe } from "react-icons/io";
+import { IoIosVideocam } from "react-icons/io";
+import { FaCogs } from "react-icons/fa";
+import { FaTrophy } from "react-icons/fa";
+import ReactTooltip from "react-tooltip";
 
 class Project extends React.Component {
-
     github() {
         if (this.props.github !== "")
-            return(
-                <a href={this.props.github}><FaGithub className="item" /></a>
+            return (
+                <a href={this.props.github} target="_blank" rel="noopener noreferrer">
+                    <FaGithub className="item" />
+                </a>
             );
     }
 
     external() {
         if (this.props.external !== "")
             return (
-                <a href={this.props.external}><IoMdOpen className="item" /></a>
+                <a href={this.props.external} target="_blank" rel="noopener noreferrer">
+                    <IoMdOpen className="item" />
+                </a>
             );
     }
 
     type() {
-        if (this.props.type === "website")
-            return (
-                <IoIosGlobe className="type" />
-            );
-        else if (this.props.type === "film")
-            return (
-                <IoIosVideocam className="type" />
-            );
-        else if (this.props.type === "hardware")
-            return (
-                <FaCogs className="type" />
-            );
+        if (this.props.type === "website") return <IoIosGlobe className="type" />;
+        else if (this.props.type === "film") return <IoIosVideocam className="type" />;
+        else if (this.props.type === "hardware") return <FaCogs className="type" />;
     }
 
     award() {
@@ -44,7 +38,7 @@ class Project extends React.Component {
             return (
                 <div>
                     <FaTrophy data-tip data-for={this.props.title} className="award" />
-                    <ReactTooltip id={this.props.title} type='success' place='right' effect='solid'>
+                    <ReactTooltip id={this.props.title} type="success" place="right" effect="solid">
                         <span>{this.props.awardText}</span>
                     </ReactTooltip>
                 </div>
@@ -52,32 +46,35 @@ class Project extends React.Component {
         }
     }
 
-    render () {
-        if(!this.props.featured)
+    render() {
+        if (!this.props.featured)
             return (
                 <div className="Project">
                     {this.award()}
                     <div className="left">
-                        <div className="container">
-                            {this.type()}
-                        </div>
+                        <div className="container">{this.type()}</div>
                     </div>
                     <div className="right">
                         <div className="container">
-                            {this.github()}{this.external()}
+                            {this.github()}
+                            {this.external()}
                         </div>
                     </div>
-                    <a href={this.props.external}><h1>{this.props.title}</h1></a>
+                    <a href={this.props.external} target="_blank" rel="noopener noreferrer">
+                        <h1>{this.props.title}</h1>
+                    </a>
                     <p>{this.props.description}</p>
                     <div className="techStack">
-                        {this.props.techStack.map((item, key) =>
-                            <p className="item" key={key}>{item}</p>
-                        )}
+                        {this.props.techStack.map((item, key) => (
+                            <p className="item" key={key}>
+                                {item}
+                            </p>
+                        ))}
                     </div>
                 </div>
             );
         else {
-            return(null);
+            return null;
         }
     }
 }
