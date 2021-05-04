275f9fae94a9d408e0a37cc0f3d3393407bc61b3
s-fletcher <sfletchertx@gmail.com>
2021-05-03 23:43:32 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio v2.0
-My second portfolio that was made with React https://v2.samfletcher.me
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Heading } from "@chakra-ui/layout"
+
+const Home = () => (
+    <Container mt={12} centerContent>
+        <Heading>Hello 👋</Heading>
+    </Container>
+)
+
+export default Home
