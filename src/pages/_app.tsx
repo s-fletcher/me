0c0abff36ff0cb9b0d0922978fbb58e40173c4c5
s-fletcher <sfletchertx@gmail.com>
2021-05-04 23:20:06 -0500

    Add basic landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
+import '@fontsource/cabin'
+import '@fontsource/martel/800.css'
 import { ChakraProvider } from '@chakra-ui/react'
-
 import { AppProps } from 'next/app'
+import { theme } from '../data/theme'
 
 function App({ Component, pageProps }: AppProps) {
-    return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
-    )
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import { Container, Heading } from "@chakra-ui/layout"
+import { Box, Container, Heading, Link, Text } from '@chakra-ui/layout'
 
 const Home = () => (
-    <Container mt={12} centerContent>
-        <Heading>Hello 👋</Heading>
-    </Container>
+  <Container maxW="container.md" mt={28} centerContent>
+    <Box>
+      <Heading fontWeight="normal" fontSize="3xl" fontFamily="mono">
+        Hello world,
+      </Heading>
+      <Heading fontSize="4xl" mt={2} ml={24}>
+        I&apos;m Sam
+      </Heading>
+    </Box>
+    <Box boxShadow="card" borderRadius="lg" mt={40} bg="white" py={10} px={20}>
+      <Heading size="lg">About</Heading>
+      <Text my={4}>
+        I&apos;m a Computer Science student at the University of Oklahoma
+        currently working an entry level Software Engineering position in
+        Oklahoma City. Unlike my coding skills, you could say I&apos;m a
+        terrible cook... but I try.
+      </Text>
+      <Text>
+        View my{' '}
+        <Link target="_blank" href="/resume.pdf">
+          resume
+        </Link>
+      </Text>
+    </Box>
+  </Container>
 )
 
 export default Home
