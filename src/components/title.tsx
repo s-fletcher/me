84530e2970da1aafde84511aafd278de02af8e1b
s-fletcher <sfletchertx@gmail.com>
2021-05-07 01:11:32 -0500

    Fix title on mobile

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -12,7 +12,12 @@ export const Title: FC<TitleProps> = ({ lineOne, lineTwo, ...props }) => (
       <Heading fontWeight="normal" fontSize="3xl" fontFamily="mono">
         {lineOne}
       </Heading>
-      <Heading fontSize="4xl" mt={2} ml={24}>
+      <Heading
+        whiteSpace="nowrap"
+        fontSize="4xl"
+        mt={2}
+        ml={lineTwo.length > 10 ? [8, 24] : 24}
+      >
         {lineTwo}
       </Heading>
     </Box>
