c84a5bf62d90f7c2a0736a4ab6fc869f518f8352
s-fletcher <sfletchertx@gmail.com>
2021-05-09 21:58:35 -0500

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,27 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          {/* favicon */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+
+          {/* fonts */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Cabin&family=Martel:wght@800&display=swap"
