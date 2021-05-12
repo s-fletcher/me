e09c5638b6f12952c9305937c680df92c5d478e1
s-fletcher <sfletchertx@gmail.com>
2021-05-11 19:13:33 -0500

    Grammar

--- src/views/work/hacklahoma/index.tsx
+++ src/views/work/hacklahoma/index.tsx
@@ -38,12 +38,12 @@ export const Hacklahoma = () => (
       <Text minW="100%">
         In the past, we’ve had challenges when it came to getting hackers
         registered and checked in. During my years with Hacklahoma, I innovated
-        this process and prevent bottlenecks and allow hackers to get hacking,
-        faster.
+        this process and prevented bottlenecks which allow hackers to get
+        hacking, faster.
       </Text>
       <Text minW="100%">
         This involved introducing an automated way of distributing QR codes to
-        hackers that were used to check them in at the event.
+        hackers which were used to check them in at the event.
       </Text>
       <Text minW="100%">
         Along with registration came collecting data and using it to benefit our
@@ -65,10 +65,10 @@ export const Hacklahoma = () => (
       <Text minW="100%">
         Hopefully you can get a good idea of what I worked on during my two
         years with Hacklahoma. Of course there was plenty more that was done
-        such as
+        such as:
         <UnorderedList pl={8} minW="100%">
           <ListItem>Creating a stable documentation system</ListItem>
-          <ListItem>Transferring domain to Cloudflare </ListItem>
+          <ListItem>Transferring domains to Cloudflare </ListItem>
           <ListItem>Leading a team of web developers</ListItem>
           <ListItem>Producing a livestream</ListItem>
           <ListItem>Developing discord bots for the virtual event</ListItem>
